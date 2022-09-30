242b016a375128c8928be8ed89f6fd350c29ae65
Seunghwan <ysp1307@naver.com>
2022-09-30 13:16:21 +0900

    🎉 프로젝트 세팅

---
+++ todots/src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return <div className="flex flex-col h-screen w-full justify-center items-center p-0 m-0" />;
+}
+
+export default App;
